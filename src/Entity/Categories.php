ba1f5a2451ee91ccda431513d806fd65da38000d
Roun1974 <diaroun74@outlook.com>
2022-05-23 22:35:41 +0200

    Adaptation controler et formType

--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -6,6 +6,7 @@ use App\Repository\CategoriesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=CategoriesRepository::class)
@@ -25,9 +26,11 @@ class Categories
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $slug;
+ * @Gedmo\Slug(fields={"name"})
+ * @ORM\Column(length=128, unique=true)
+ */
+private $slug;
+   
 
     /**
      * @ORM\ManyToOne(targetEntity=Categories::class, inversedBy="categories")
@@ -70,13 +73,6 @@ class Categories
     public function getSlug(): ?string
     {
         return $this->slug;
-    }
-
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
     }
 
     public function getParent(): ?self
