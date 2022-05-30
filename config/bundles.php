45bcac0591f21ec6b8680c1d00d85636dbf6a228
Roun1974 <diaroun74@outlook.com>
2022-05-30 22:49:56 +0200

    Profile page

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
